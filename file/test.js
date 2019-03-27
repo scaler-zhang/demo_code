c5ee0ccbed3d33bc73e03ce54d62f92d95f7e890
scaler-zhang <zhangbin2013@aliyun.com>
2019-03-27 23:28:27 +0800

    Initial commit

---
+++ file/test.js
@@ -0,0 +1,8 @@
+/**
+ *Created by bin on 2018/12/30
+ */
+document.querySelector('div').onclick = function () {
+    this.style.width = '300px';
+    this.style.height = '300px';
+    this.style.backgroundColor = 'yellow';
+}
